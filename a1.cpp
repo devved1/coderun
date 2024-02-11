90b69cce1a80ab78f66be8c0a86ff07b1fb706e9
Dev Mahajan <devm4387@gmail.com>
2024-02-11 20:54:11 +0530

    editorial

---
+++ a1.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+
+
+void solve(){
+
+int n;cin>>n;
+if(n==0){
+    cout<<1<<"\n";return;
+}
+cout<<3*n<<"\n";
+        
+}
+
+
+int32_t main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);cout.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
